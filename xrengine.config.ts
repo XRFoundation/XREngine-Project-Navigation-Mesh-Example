65eef7c2513daa25b13a69d9335dd333788977ef
Patrick <pscale01@gmail.com>
2021-11-30 15:35:42 -0800

    XREngine##3883: update route

--- xrengine.config.ts
+++ xrengine.config.ts
@@ -4,7 +4,7 @@ const config: ProjectConfigInterface = {
   onEvent: undefined,
   thumbnail: '/static/xrengine_thumbnail.jpg',
   routes: {
-    '/': {
+    '/foo': {
       component: () => import('./index'),
       props: {
         exact: true
